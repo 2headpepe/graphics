54ea27a84af8709c8213bdad3346da39b4c59c7d
2headpepe <st095279@student.spbu.ru>
2023-04-24 15:28:21 +0300

    add scaling

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,47 @@
 main();
-
 function main() {
   const coef = getCoef();
+  console.log("coef");
+  console.log(coef);
   let roots = new Set(solve(coef.a, coef.b, coef.c));
   roots.add(0);
-
+  console.log("roots");
+  console.log(roots);
   let borders = getBorder(roots, coef);
 
   const canvas = document.getElementById("canvas");
   if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(50, 100);
+    ctx.lineTo(150, 100);
+    ctx.stroke();
     const cellSize = getCellSize(
       borders.leftX,
       borders.rightX,
       borders.bottomY,
       borders.topY
     );
-
+    console.log("cellSize");
+    console.log(cellSize);
+    console.log("scale");
+    console.log(
+      getScale(borders.leftX, borders.rightX, borders.bottomY, borders.topY)
+    );
+    let scale = getScale(
+      borders.leftX,
+      borders.rightX,
+      borders.bottomY,
+      borders.topY
+    );
     canvas.height = (borders.topY - borders.bottomY) * cellSize;
     canvas.width = (borders.rightX - borders.leftX) * cellSize;
     console.log(borders);
     const coordinatesCenter = getCenterCoordinates(
       cellSize,
       borders.leftX,
-      borders.topY
+      borders.topY,
+      scale
     );
     drawCoordinateLines(
       cellSize,
@@ -32,7 +50,8 @@ function main() {
       borders.rightX,
       borders.bottomY,
       borders.topY,
-      coordinatesCenter
+      coordinatesCenter,
+      scale
     );
 
     drawFunc(
@@ -42,7 +61,8 @@ function main() {
       borders.leftX,
       borders.rightX,
       borders.bottomY,
-      borders.topY
+      borders.topY,
+      scale
     );
   }
 }
@@ -54,13 +74,30 @@ function getCoef() {
   // let a = +prompt("Input a: ", 0);
   // let b = +prompt("Input b: ", 0);
   // let c = +prompt("Input c: ", 0);
-  return { a: -1, b: -4, c: -10 };
+  let ans = prompt(
+    `Enter "y" if you want enter coefficients
+(by default it is random values from -50 to 50)`,
+    ""
+  );
+  if (ans != "y") {
+    return {
+      a: Math.random() * 100 - 50,
+      b: Math.random() * 100 - 50,
+      c: Math.random() * 100 - 50,
+    };
+  } else {
+    a = +prompt("okay! Enter first coefficient!", 0);
+    b = +prompt("Enter second coefficient!", 0);
+    c = +prompt("Enter third coefficient!", 0);
+    return { a, b, c };
+  }
 }
 
 /**Function for solving quadratic equation: ax^2 + bx + c = 0
  * @returns {Array} Roots
  */
 function solve(a, b, c) {
+  // console.log([a, b, c]);
   const D = Math.pow(b, 2) - 4 * a * c;
   if (D < 0) return [];
   if (D == 0) return [-b / 2 / a];
@@ -76,18 +113,26 @@ function getBorder(roots, coef) {
   let leftX = Math.min(...roots);
   let rightX = Math.max(...roots);
   // console.log([leftX, rightX]);
-  let topY;
-  let bottomY;
+  let topY = 10;
+  let bottomY = -10;
 
   const xv = -coef.b / 2 / coef.a;
-  const yv = coef.a * xv * xv + coef.b * xv + coef.c;
+  const yv = f(coef, xv);
   // console.log([yv, xv]);
   if (yv < 0) {
-    bottomY = yv * 2;
-    topY = bottomY < -10 ? -yv / 2 : 10 + bottomY;
-  } else {
-    topY = yv * 2;
-    bottomY = topY > 10 ? -yv / 2 : -10 + yv;
+    bottomY = yv * 1.1;
+    if (bottomY < -50) {
+      topY = 10;
+    } else {
+      topY = bottomY < -10 ? -yv / 2 : 10 + bottomY;
+    }
+  } else if (yv > 0) {
+    topY = yv * 1.1;
+    if (top > 50) {
+      bottomY = -10;
+    } else {
+      bottomY = topY > 10 ? -yv / 2 : -10 + yv;
+    }
     // maximumY = yv;
   }
   // console.log([leftX, rightX, bottomY, topY]);
@@ -106,7 +151,9 @@ function getBorder(roots, coef) {
   leftX = Math.ceil(leftX * 1.1);
   bottomY = Math.floor(bottomY * 1.1);
   topY = Math.ceil(topY * 1.1);
-  // console.log([leftX, rightX, bottomY, topY]);
+
+  console.log("[leftX, rightX, bottomY, topY]");
+  console.log([leftX, rightX, bottomY, topY]);
   return { leftX, rightX, bottomY, topY };
 }
 
@@ -118,10 +165,23 @@ function getBorder(roots, coef) {
  * @returns {Number} Size of cell
  */
 function getCellSize(leftX, rightX, bottomY, topY) {
-  return Math.min(
-    window.innerWidth / (rightX - leftX) / 2,
-    window.innerHeight / (topY - bottomY) / 2
+  // console.log([leftX, rightX, bottomY, topY]);
+  let dif = rightX - leftX;
+  let tmpSize = Math.min(
+    window.innerWidth / (rightX - leftX),
+    window.innerHeight / (topY - bottomY)
   );
+  console.log(window.innerWidth);
+  console.log(dif / 20);
+  let scale = Math.ceil(dif / 20);
+  console.log(scale);
+  tmpSize *= scale;
+  return tmpSize;
+}
+function getScale(leftX, rightX) {
+  let dif = rightX - leftX;
+
+  return Math.ceil(dif / 20);
 }
 
 /**Function for getting size of cells
@@ -130,8 +190,8 @@ function getCellSize(leftX, rightX, bottomY, topY) {
  * @param {Number} topY - top border
  * @returns {Object} X and Y coordinates of center
  */
-function getCenterCoordinates(cellSize, leftX, topY) {
-  return { x: -leftX * cellSize, y: topY * cellSize };
+function getCenterCoordinates(cellSize, leftX, topY, scale) {
+  return { x: (-leftX * cellSize) / scale, y: (topY * cellSize) / scale };
 }
 
 /**Draws coordinate lines
@@ -150,22 +210,103 @@ function drawCoordinateLines(
   rightX,
   bottomY,
   topY,
-  coordinatesCenter
+  coordinatesCenter,
+  scale
 ) {
   console.log("Center:");
   console.log(coordinatesCenter);
-  ctx.fillStyle = "#808080";
-  for (let y = 0; y < (topY - bottomY) * cellSize; y += cellSize) {
-    ctx.fillRect(0, y, cellSize * (rightX - leftX), 1);
-  }
-  for (let x = 0; x < (rightX - leftX) * cellSize; x += cellSize) {
-    ctx.fillRect(x, 0, 1, cellSize * (topY - bottomY));
-  }
   ctx.fillStyle = "#000000";
-  ctx.fillRect(0, coordinatesCenter.y, cellSize * (rightX - leftX), 1);
-  ctx.fillRect(coordinatesCenter.x, 0, 1, cellSize * (topY - bottomY));
-}
+  ctx.fillRect(
+    0,
+    coordinatesCenter.y,
+    (cellSize * (rightX - leftX)) / scale,
+    1
+  );
+  ctx.fillRect(
+    coordinatesCenter.x,
+    0,
+    1,
+    (cellSize * (topY - bottomY)) / scale
+  );
 
+  let i = 1;
+  for (let y = coordinatesCenter.y - cellSize; y >= 0; y -= cellSize) {
+    ctx.fillStyle = "#808080";
+    ctx.fillRect(0, y, (cellSize * (rightX - leftX)) / scale, 1);
+    // ctx.fillStyle = "#ffffff";
+    // ctx.fillRect(coordinatesCenter.x, y, cellSize, 10);
+    ctx.fillStyle = "#000000";
+    ctx.font = "bold 10px serif";
+
+    ctx.fillText(
+      i * scale,
+      coordinatesCenter.x + cellSize / 10,
+      y - cellSize / 10,
+      (cellSize * 9) / 10
+    );
+    ++i;
+  }
+
+  i = 0;
+  for (
+    let y = coordinatesCenter.y;
+    y < ((topY - bottomY) * cellSize) / scale;
+    y += cellSize
+  ) {
+    // console.log("hi");
+
+    ctx.fillStyle = "#808080";
+    ctx.fillRect(0, y, (cellSize * (rightX - leftX)) / scale, 1);
+    ctx.fillStyle = "#000000";
+    ctx.font = "bold 10px serif";
+
+    ctx.fillText(
+      -i * scale,
+      coordinatesCenter.x + cellSize / 10,
+      y - cellSize / 10,
+      (cellSize * 9) / 10
+    );
+    ++i;
+  }
+
+  i = 1;
+  for (
+    let x = coordinatesCenter.x + cellSize;
+    x < ((rightX - leftX) * cellSize) / scale;
+    x += cellSize
+  ) {
+    ctx.fillStyle = "#808080";
+    ctx.fillRect(x, 0, 1, (cellSize * (topY - bottomY)) / scale);
+    ctx.fillStyle = "#000000";
+    ctx.font = "bold 10px serif";
+
+    ctx.fillText(
+      i * scale,
+      x + cellSize / 10,
+      coordinatesCenter.y - cellSize / 10,
+      (cellSize * 9) / 10
+    );
+    ++i;
+  }
+  i = 1;
+  for (let x = coordinatesCenter.x - cellSize; x >= 0; x -= cellSize) {
+    ctx.fillStyle = "#808080";
+    ctx.fillRect(x, 0, 1, (cellSize * (topY - bottomY)) / scale);
+    ctx.fillStyle = "#000000";
+    ctx.font = "bold 10px serif";
+
+    ctx.fillText(
+      -i * scale,
+      x + cellSize / 10,
+      coordinatesCenter.y - cellSize / 10,
+      (cellSize * 9) / 10
+    );
+    ++i;
+  }
+}
+function f(coef, x) {
+  return coef.a * x * x + coef.b * x + coef.c;
+}
 /**Draws function graph
  * @param {Object} coef - object with coefficients a:Number,b:Number,c:Number
  * @param {Number} cellSize - Size of cell
@@ -175,22 +316,42 @@ function drawCoordinateLines(
  * @param {Number} bottomY - bottom border
  * @param {Number} topY - top border
  */
-function drawFunc(coef, cellSize, ctx, leftX, rightX, bottomY, topY) {
+function drawFunc(coef, cellSize, ctx, leftX, rightX, bottomY, topY, scale) {
   // console.log([coef, ctx, cellSize, leftX, bottomY, topY, rightX]);
   ctx.fillStyle = "#000000";
-  // let h=1e-10;
   let dif = 2 * coef.a * leftX + coef.b;
+  let cosx = Math.cos(Math.atan(dif));
+  let sinx = Math.sin(Math.atan(dif));
+  let h = 1 / 20;
+  let i = 0;
   // console.log([dif,leftX,topY]);
-  for (let x = leftX; x <= rightX; x += 1 / 10 / Math.abs(dif) / cellSize) {
-    let dif = 2 * coef.a * x + coef.b;
-    // alert(x);
-    let y = coef.a * x * x + coef.b * x + coef.c;
+  for (let x = leftX; x <= rightX; x += h * cosx) {
+    dif = 2 * coef.a * x + coef.b;
+    cosx = Math.cos(Math.atan(dif));
+    let y = f(coef, x);
     if (y > topY || y < bottomY) continue;
-    let coordinates = changeCoordinates(cellSize, x, y, leftX, topY);
-    ctx.fillRect(coordinates.x, coordinates.y, 0.5, 0.5);
+    let tmpCoordinates = changeCoordinates(
+      cellSize,
+      x,
+      f(coef, x),
+      leftX,
+      topY,
+      scale
+    );
+    let newCoordinates = changeCoordinates(
+      cellSize,
+      x + h * cosx,
+      f(coef, x + h * cosx),
+      leftX,
+      topY,
+      scale
+    );
+    ctx.moveTo(tmpCoordinates.x, tmpCoordinates.y);
+    ctx.lineTo(newCoordinates.x, newCoordinates.y);
 
     // console.log(x);
   }
+  ctx.stroke();
 }
 
 /**Function for converting default mathematic coordinates to computer
@@ -201,10 +362,17 @@ function drawFunc(coef, cellSize, ctx, leftX, rightX, bottomY, topY) {
  * @param {Number} topY - top border
  * @returns {Object} X and Y coordinates
  */
-function changeCoordinates(cellSize, coordinateX, coordinateY, leftX, topY) {
-  let centerCoordinates = getCenterCoordinates(cellSize, leftX, topY);
+function changeCoordinates(
+  cellSize,
+  coordinateX,
+  coordinateY,
+  leftX,
+  topY,
+  scale
+) {
+  let centerCoordinates = getCenterCoordinates(cellSize, leftX, topY, scale);
   return {
-    x: centerCoordinates.x + cellSize * coordinateX,
-    y: centerCoordinates.y - cellSize * coordinateY,
+    x: centerCoordinates.x + (cellSize * coordinateX) / scale,
+    y: centerCoordinates.y - (cellSize * coordinateY) / scale,
   };
 }
